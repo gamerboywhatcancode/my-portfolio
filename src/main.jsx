3de0263f4a86c27298c6ad48942be0053e98b435
gamerboywhatcancode <faarisbinrezapc@gmail.com>
2023-09-27 19:10:08 +0600

    yes

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Link } from 'react-router-dom'
+import Home from './components/home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/'>
+    <Link to='/'/>
     <App />
   </BrowserRouter>,
 )
