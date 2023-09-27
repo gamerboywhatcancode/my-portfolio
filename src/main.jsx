a06b368fb97fb168de115b03319152acaa28ef4f
gamerboywhatcancode <faarisbinrezapc@gmail.com>
2023-09-27 19:55:45 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter, Link } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Link } from 'react-router-dom'
 import Home from './components/home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/'>
-    <Link to='/'/>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>
+    
+  
 )
